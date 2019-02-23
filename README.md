4f23e0fae25a09870a0e662f2c52ebf233fb7e88
SophieM <47410186+SophM@users.noreply.github.com>
2019-02-22 22:19:23 -0800

    add final version of files - styling and fun-fact-picture element added

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Word-Guess Game
 
-In this project, I built a word-guess game, whose goal is for the player to guess a word.
+In this project, I built a word-guess game, whose goal is for the player to guess a word and learn new things! In fact, if the player guesses the word, a picture representing the word and a fun fact about the word are displayed.
 
 [Click me to check the game out!](https://sophm.github.io/Word-Guess-Game/)
 
 The word to guess (with hidden letters!) is displayed on the screen, along with the score of the player (wins/losses), the letters the player already tried and the number of guesses remaining. The player has up to 10 guesses to find the word!
 
-A round ends when the player finds the word or when she/he doesn't have anymore guesses left. At the end of a round, the game starts over automatically and a new word to guess is displayed on the screen.
+A round ends when the player finds the word or when she/he doesn't have anymore guesses left. At the end of a round, the game starts over automatically and a new hidden-word to guess is displayed on the screen.
 
-To avoid the same word to be picked up twice, the chosen word is removed from the word-to-guess list.
+To avoid the same word to be picked up twice, every selected word is removed from the word-to-guess list.
 
 To build this game, I used the following technologies:
 - HTML
@@ -19,6 +19,13 @@ Below is a screenshot of the game.
 
 ---
 
-### Screenshot of the game
+### Screenshot of the game at the start
 
-![Screenshot of the game]()
+![Screenshot of the game](https://github.com/SophM/Word-Guess-Game/blob/master/assets/screenshots_for_readme/Screenshot_beginning_of_the_game.png?raw=true)
+
+
+---
+
+### Screenshot of the game, after the word "hippopotamus" has been guessed and a new round is ready to start.
+
+![Screenshot of the game when the word has been guessed](https://github.com/SophM/Word-Guess-Game/blob/master/assets/screenshots_for_readme/Screenshot_word_guessed.png?raw=true)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 // ==================================
 
 // array with the possible words to guess
-var possibleWords = ["mountain", "shrimp", "squirrel","chimpanzee", "swamp", "savannah", "tundra", "hamster", "bumblebee", "owl", "platypus", "forest", "salamander", "taiga", "hippopotamus"];
+var possibleWords = ["mountain", "shrimp", "squirrel", "chimpanzee", "swamp", "savanna", "tundra", "hamster", "bumblebee", "owl", "platypus", "forest", "meerkat", "salamander", "hippopotamus"];
 // variable to keep track of the wins
 var wins = 0;
 // variable to keep track of the losses
@@ -22,6 +22,44 @@ var selectedWord;
 var wordInArray;
 // variable to store the player's guess
 var playerGuess;
+
+// // Array to associate a picture to the selected word when the player guessed it
+var pictures = [
+    {w: "mountain", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/mountains.jpg?raw=true"},
+    {w: "shrimp", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/shrimp.jpg?raw=true"},
+    {w: "squirrel", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/squirrel.jpg?raw=true"},
+    {w: "chimpanzee", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/chimpanzee.jpg?raw=true"},
+    {w: "swamp", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/swamp.jpg?raw=true"},
+    {w: "savannah", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/savannah.jpg?raw=true"},
+    {w: "tundra", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/tundra.jpg?raw=true"},
+    {w: "hamster", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/hamster.jpg?raw=true"},
+    {w: "bumblebee", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/bumblebee.jpg?raw=true"},
+    {w: "owl", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/owl.jpg?raw=true"},
+    {w: "platypus", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/Duckbilled_platypus.jpg?raw=true"},
+    {w: "forest", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/forest.jpg?raw=true"},
+    {w: "meerkat", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/meerkat.jpg?raw=true"},
+    {w: "salamander", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/salamander.jpg?raw=true"},
+    {w: "hippopotamus", s: "https://github.com/SophM/Word-Guess-Game/blob/master/assets/images/hippo.jpg?raw=true"},
+]
+
+// Array to associate a fun fact about the selected word when the player guessed it
+var facts = [
+    {w: "mountain", s: "About 80% of our planet's fresh water originates in the mountains."},
+    {w: "shrimp", s: "Several shrimp species, known as cleaners, safely venture inside the open mouths of fishes to remove bloodsucking parasites."},
+    {w: "squirrel", s: "Squirrels can find food buried beneath a foot of snow. They usually don’t dig up all of their buried nuts, which results in more trees!"},
+    {w: "chimpanzee", s: "We, Homo sapiens, share 98.4% of our DNA with chimpanzees."},
+    {w: "swamp", s: "Swamps are like nature's paper towels because they soak up water when it rains a lot. They help keep the land around them from flooding."},
+    {w: "savanna", s: "During the wet season, which lasts about six months, a savanna can get anywhere from 20-50 inches of rain."},
+    {w: "tundra", s: "Trundras are the coldest and one of the largest ecosystems on Earth. They cover about one-fifth of the land on the planet."},
+    {w: "hamster", s: "Hamsters' top speed: 4 mph. They run as fast as humans walk!"},
+    {w: "bumblebee", s: "Bumblebees' metabolism is so fast that they’re always only about 40 minutes from starving – even on a full stomach!"},
+    {w: "owl", s: "Unlike most other owls, snowy owls are are active and hunt during the day and night."},
+    {w: "platypus", s: "It is one of the few venomous mammals; the male Platypus has a spur on the hind foot, which delivers a poison capable of causing severe pain to humans."},
+    {w: "forest", s: "More than 25% of the medicines we use originate in rainforest plants"},
+    {w: "meerkat", s: "The baby meerkats go to school like us! It is where they learn how to eat poisonous scorpions, their favorite food!"},
+    {w: "salamander", s: "Salamanders have super-powers. They can regenerate their brain, heart, parts of their eyes, limbs and tails!"},
+    {w: "hippopotamus", s: "Hippos are great swimmers. They can hold their breath for up to 5 minutes underwater!"},
+]
 
 
 // ==================================
@@ -116,6 +154,24 @@ function resetNbLettersGuessed() {
     numberGuessed = 0;
 };
 
+// function to associate the picture representing the selected word if the word is guessed
+function displayPicture(word) {
+    for (var i = 0; i < pictures.length; i++) {
+        if (pictures[i].w === word) {
+            document.getElementById("pic").setAttribute("src", pictures[i].s);
+        }
+    }
+};
+
+// function to associate the fun fact about the selected word if the word is guessed
+function displayFact(word) {
+    for (var i = 0; i < facts.length; i++) {
+        if (facts[i].w === word) {
+            document.getElementById("fun-fact").innerHTML = facts[i].s;
+        }
+    }
+};
+
 
 // ==================================
 // Main process
@@ -194,6 +250,10 @@ document.onkeyup = function(event) {
                 // the number of wins increases by 1 and
                 // the new number is displayed on the screen
                 updateWins();
+                // a picture represented the word guessed is displayed
+                displayPicture(selectedWord);
+                // a fun fact about the word guessed is displayed
+                displayFact(selectedWord);
                 // the game starts over...
                 // the number of letters guessed is set to zero
                 resetNbLettersGuessed();
